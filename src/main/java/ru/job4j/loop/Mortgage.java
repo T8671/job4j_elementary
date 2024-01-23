165b3553cadafaf956c53cfe8f978b8053a57f40
Teymur <azimovta@gmail.com>
2024-01-23 23:02:23 +0500

    5.7. Ипотека [#156315 #475427]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double debt = amount * percent / 100 + amount;
-        while (debt > 0) {
-            debt -= salary;
+        while (amount > 0) {
+            amount = amount * percent / 100 + amount;
+            amount -= salary;
             year++;
-            debt = debt * percent / 100 + debt;
+
         }
         return year;
     }
