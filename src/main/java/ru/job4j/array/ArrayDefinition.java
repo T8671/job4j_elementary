cbcc96fac8fce4f1936836597cc4d8515b51addb
Teymur <azimovta@gmail.com>
2024-02-23 17:55:49 +0500

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-
         String[] names = new String[4];
         names[0] = "Jesse Heiman";
         names[1] = "Leonardo DiCaprio";
         names[2] = "Johnny Depp";
         names[3] = "Brad Pitt";
-
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
