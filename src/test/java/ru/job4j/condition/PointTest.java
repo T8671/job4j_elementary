e12a98cc9d593dceb4a085001f54166dba413b58
Teymur <azimovta@gmail.com>
2024-03-03 18:28:21 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,62 +1,53 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
 
 class PointTest {
 
     @Test
-    void when00to20Then2() {
+    public void when00to20Then2() {
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 0);
+        a.distance(b);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void when0Minus2to25Then4Dot24() {
+    void when1Minus2to25Then4Dot24() {
+        Point a = new Point(1, 2);
+        Point b = new Point(-2, 5);
+        a.distance(b);
         double expected = 4.24;
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = -2;
-        int y2 = 5;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when1Minus5toMinus51Then8Dot48() {
+        Point a = new Point(1, -5);
+        Point b = new Point(-5, 1);
+        a.distance(b);
         double expected = 8.48;
-        int x1 = 1;
-        int y1 = -5;
-        int x2 = -5;
-        int y2 = 1;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when18to29Then9dot89() {
+        Point a = new Point(1, 2);
+        Point b = new Point(8, 9);
+        a.distance(b);
         double expected = 9.89;
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 8;
-        int y2 = 9;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenMinus1Minus8toMinus8Minus9Then9dot89() {
+    void whenMinus1Minus8toMinus2Minus9Then9dot89() {
+        Point a = new Point(-1, -2);
+        Point b = new Point(-8, -9);
+        a.distance(b);
         double expected = 9.89;
-        int x1 = -1;
-        int y1 = -2;
-        int x2 = -8;
-        int y2 = -9;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 }
